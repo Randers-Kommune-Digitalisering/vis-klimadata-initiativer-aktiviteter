00a84db0f7b01ef5cd4f1872f82d7026f438b5fc
st-randers <115066094+st-randers@users.noreply.github.com>
2023-08-03 11:28:35 +0000

    Added both empty and null check to OS2IoT flow Attempt to bug fix to OS2IoT JSONata date parsing error.

--- node-red/flows_js/switch.8203e8b6d8a64094.flows.js
+++ node-red/flows_js/switch.8203e8b6d8a64094.flows.js
@@ -1,32 +1,32 @@
 const Node = {
-  "id": "dc300126a2092f0d",
+  "id": "8203e8b6d8a64094",
   "type": "switch",
   "z": "aad50484a0b40b44",
-  "name": "Er data tom?",
-  "property": "payload[0].last_observation ~> $exists()",
-  "propertyType": "jsonata",
+  "name": "Er data null?",
+  "property": "payload[0].last_observation",
+  "propertyType": "msg",
   "rules": [
     {
-      "t": "false"
+      "t": "null"
     },
     {
-      "t": "true"
+      "t": "nnull"
     }
   ],
   "checkall": "true",
   "repair": false,
   "outputs": 2,
-  "x": 330,
-  "y": 900,
+  "x": 350,
+  "y": 940,
   "wires": [
     [
       "edcd9921993e19bf"
     ],
     [
-      "8203e8b6d8a64094"
+      "8e3bb04c7e0fbde5"
     ]
   ],
-  "_order": 464
+  "_order": 493
 }
 
 module.exports = Node;
